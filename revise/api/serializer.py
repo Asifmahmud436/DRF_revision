b8fa4b95a93b9444e6a78b1da616fdfdb4949c58
Zettex <safaandsafa4@gmail.com>
2025-06-13 01:11:00 +0600

    learned modelserializer

--- revise/api/serializer.py
+++ revise/api/serializer.py
@@ -1,29 +1,48 @@
 from rest_framework import serializers
 from .models import Student
-class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=200)
-    city = serializers.CharField(max_length=200)
-    roll = serializers.IntegerField()
+
+# class StudentSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=200)
+#     city = serializers.CharField(max_length=200)
+#     roll = serializers.IntegerField()
     
-    def validate_roll(self,value):
-        if value>100:
-            raise serializers.ValidationError("Roll cannot be greater than 100!")
-        return value
+#     def validate_roll(self,value):
+#         if value>100:
+#             raise serializers.ValidationError("Roll cannot be greater than 100!")
+#         return value
     
-    def validate(self, data):
-        name = data.get('name')
-        city = data.get('city')
-        if name.lower() == 'rohit' and city.lower()!= 'india':
-            raise serializers.ValidationError("Rohit is an Indian!")
-        return data
+#     def validate(self, data):
+#         name = data.get('name')
+#         city = data.get('city')
+#         if name.lower() == 'rohit' and city.lower()!= 'india':
+#             raise serializers.ValidationError("Rohit is an Indian!")
+#         return data
         
     
-    def create(self, validated_data):
-        return Student.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Student.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name',instance.name)
-        instance.roll = validated_data.get('roll',instance.roll)
-        instance.city = validated_data.get('city',instance.city)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name',instance.name)
+#         instance.roll = validated_data.get('roll',instance.roll)
+#         instance.city = validated_data.get('city',instance.city)
+#         instance.save()
+#         return instance
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id','name','roll','city']
+        read_only_fields = ['id']
+    
+    def validate_roll(self,value):
+        if value > 200:
+            raise serializers.ValidationError("Can not admit more than 200 students")
+        return value
+    
+    def validate(self,data):
+        name = data.get('name')
+        city = data.get('city')
+        if city.lower() == 'israel':
+            raise serializers.ValidationError("There is no country named Israel")
+        return data
--- revise/myapp.py
+++ revise/myapp.py
@@ -5,7 +5,7 @@ URL = "http://127.0.0.1:8000/studentApi/"
 
 def get_data(id=None):
     data = {}
-    if id:
+    if id is not None:
         data = {'id':id}
     json_data = json.dumps(data)
     response = requests.get(url = URL, data = json_data)
@@ -43,4 +43,7 @@ def delete_data():
     response = requests.delete(url=URL, data=json_data)
     print(response.json())
     
-post_data()
+# post_data()
+get_data()
+# update_data()
+# delete_data()

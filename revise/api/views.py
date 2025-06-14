3cd2a5bdf107a87693a5add9ea7b7f5bccaf001f
Zettex <safaandsafa4@gmail.com>
2025-06-14 14:07:17 +0600

    learned basic auth

--- revise/api/views.py
+++ revise/api/views.py
@@ -1,7 +1,11 @@
 from .models import Student
 from .serializer import StudentSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny
 
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    authentication_classes = BasicAuthentication
+    permission_classes = [IsAuthenticated]

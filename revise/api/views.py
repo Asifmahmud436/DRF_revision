f38d7d95e1261332053e4eaa282b8cf1c38d28cd
Zettex <safaandsafa4@gmail.com>
2025-06-14 21:44:06 +0600

    learned jwt auth

--- revise/api/views.py
+++ revise/api/views.py
@@ -1,11 +1,10 @@
 from .models import Student
 from .serializer import StudentSerializer
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import BasicAuthentication,TokenAuthentication
 from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = BasicAuthentication
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
--- revise/revise/urls.py
+++ revise/revise/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = DefaultRouter()
 router.register('students',views.StudentViewSet,basename='student')
@@ -9,5 +11,10 @@ router.register('students',views.StudentViewSet,basename='student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls')),
+    path('gettoken/', obtain_auth_token),
+    path('login/',TokenObtainPairView.as_view()),
+    path('refresh/',TokenRefreshView.as_view()),
+    path('verify/',TokenVerifyView.as_view()),
     
 ]
